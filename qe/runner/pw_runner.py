581a70acef3a37e8470a5e1802bc6727992731e8
Mikhail <tsitsvero@gmail.com>
2022-12-27 14:22:06 +0900

    pw_runner

--- qe/runner/pw_runner.py
+++ qe/runner/pw_runner.py
@@ -1,12 +1,64 @@
 import os
 import subprocess
 
+# from ase.io import read, write
 
-def run_pw_scf(calc_directory):
-    raise NotImplementedError
+from ase.build import bulk
+from ase.calculators.espresso import Espresso
 
 
-def run_gipaw(calc_directory):
+import os
+from datetime import datetime
+
+def make_calc_dir():
+    dir_name = f'{datetime.now().strftime("%Y-%m-%d_%I %M-%S_%p")}'
+    dir_name = 'test/' + dir_name
+    os.makedirs(dir_name, exist_ok=False)
+    abs_dir_path = os.path.abspath("src/examplefile.txt")
+    return abs_dir_path
+
+
+
+# https://sites.google.com/site/dceresoli/pseudopotentials
+pseudopotentials = {'Na': 'Na.pbe-tm-gipaw-dc.UPF',
+                    'Cl': 'Cl.pbe-tm-gipaw.UPF',
+                    'Cu': 'Cu.pbe-tm-new-gipaw.UPF',
+                    'Sb': 'sb_pbe_v1.4.uspp.F.UPF', # copied from another folder
+                    'N': 'N.pbe-tm-new-gipaw-dc.UPF',
+                    'F': 'F.pbe-tm-new-gipaw-dc.UPF',
+                    'Si': 'Si.pbe-tm-new-gipaw-dc.UPF',
+                    'C': 'C.pbe-tm-new-gipaw-dc.UPF',
+                    'H': 'H.pbe-tm-new-gipaw-dc.UPF'
+                    }
+
+
+def run_pw_scf(calc_directory, structure, num_proc_pw):
+
+    os.environ["ASE_ESPRESSO_COMMAND"] = "mpirun --oversubscribe -np " + str(num_proc_pw) + " pw.x -in PREFIX.pwi > PREFIX.pwo"
+    os.environ["OMP_NUM_THREADS"] = "1,1"
+
+    os.chdir(calc_directory)
+
+    pseudo_dir = '../../Pseudopotentials'
+
+    calc = Espresso(pseudopotentials=pseudopotentials, pseudo_dir = pseudo_dir,
+    outdir = './outdir',  prefix = 'crystal', restart_mode = 'from_scratch',
+                    tstress=True, tprnfor=True, nosym=True, 
+                    ecutwfc=50, kpts=(1, 1, 1), 
+                    ecutrho = 500,
+                    occupations = 'smearing', smearing = 'gauss', degauss = 1.0e-2
+                    )
+
+    crystal.calc = calc
+
+    print(crystal.get_potential_energy())
+    # print(rocksalt.get_forces())
+
+    return
+
+
+
+def run_gipaw(calc_directory, num_proc_gipaw):
     # &inputgipaw
     #     job = 'nmr'
     #     prefix = 'TMS'
@@ -19,7 +71,7 @@ def run_gipaw(calc_directory):
     os.chdir(calc_directory)
 
 
-    num_proc_gipaw = 6
+    # num_proc_gipaw = 6
     gipaw_input = """&inputgipaw
         job = 'nmr'
         prefix = 'crystal'
@@ -38,7 +90,6 @@ def run_gipaw(calc_directory):
     # f = open("espresso_gipaw.pwo", "a")
     # subprocess.run(["mpirun --oversubscribe -np " + str(num_proc_gipaw) + " gipaw.x -in espresso_gipaw.pwi > espresso_gipaw.pwo"], shell=True)
     subprocess.run(["mpirun -np " + str(num_proc_gipaw) + " gipaw.x -in espresso_gipaw.pwi > espresso_gipaw.pwo"], shell=True)
-
 
 
     return
